ae6728f0269419edc6ba67255850e33880702cb2
Roshan Zambare <124487460+Roshan21312@users.noreply.github.com>
2026-04-04 23:57:33 +0530

    Add files via upload

---
+++ Feeling_Bad/Princess.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("My Dear Princess !!")
+st.write(
+    "Don't worry whatever is bothering you,\n"
+    "you don't have to go through it alone. I am always there for you, Okayy?"
+)
+
+st.image(r"D:\sending-love.gif") 
