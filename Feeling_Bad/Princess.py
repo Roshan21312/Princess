1180af8ed82faf5d1f899bfc3257fbc27af18c46
Roshan Zambare <124487460+Roshan21312@users.noreply.github.com>
2026-04-05 01:49:35 +0530

    Update Princess.py

--- Feeling_Bad/Princess.py
+++ Feeling_Bad/Princess.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("My Dear Princess !!")
+st.title("My Dear Princess Sanmati !!")
 st.write(
     "Don't worry whatever is bothering you,\n"
     "you don't have to go through it alone. I am always there for you, Okayy?"
