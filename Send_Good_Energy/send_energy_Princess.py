d84456fc85a5603f7e8d53ef3595bd32f2eb3245
Roshan Zambare <124487460+Roshan21312@users.noreply.github.com>
2026-05-01 22:06:21 +0530

    Update send_energy_Princess.py

--- Send_Good_Energy/send_energy_Princess.py
+++ Send_Good_Energy/send_energy_Princess.py
@@ -3,14 +3,8 @@ import streamlit as st
 st.title("My Dear Princess Sanmati !!")
 st.write(
     "I know you were angry and now you are in No mood",
-  "Here is some good energy, Sending you lots of energy and love"
+    "Here is some good energy, Sending you lots of energy and love"
 )
 
-st.image(r"Send_Good_Energy/baby-bear-brown.gif") 
-st.markdown(
-        f'<div style="display: flex; justify-content: center;">'
-        f'<img src="Send_Good_Energy/baby-bear-brown.gif">'
-        f'</div>',
-        unsafe_allow_html=True,
-    )
+st.image(r"Send_Good_Energy/cute-mochi-mochi.gif") 
 st.audio(r"Send_Good_Energy/Hall_Of_Fame.mp3")
