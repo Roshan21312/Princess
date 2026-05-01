ef521caf09e62c1726fb906e1a820ea75488b4ca
Roshan Zambare <124487460+Roshan21312@users.noreply.github.com>
2026-05-01 22:12:03 +0530

    Update send_energy_Princess.py

--- Send_Good_Energy/send_energy_Princess.py
+++ Send_Good_Energy/send_energy_Princess.py
@@ -11,5 +11,5 @@ col1, col2, col3 = st.columns([1, 2, 1])  # middle column is wider
 with col2:
     st.image(r"Send_Good_Energy/baby-bear-brown.gif") 
 
-Anddddd, Here's a good motivating energetic song for you
+st.write("Anddddd, Here's a good motivating energetic song for you")
 st.audio(r"Send_Good_Energy/Hall_Of_Fame.mp3")
