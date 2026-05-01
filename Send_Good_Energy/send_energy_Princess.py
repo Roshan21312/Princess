25474eb715be448d40e8cfe9af15233c9f59bfa2
Roshan Zambare <124487460+Roshan21312@users.noreply.github.com>
2026-05-01 21:53:40 +0530

    Create send_energy_Princess.py

---
+++ Send_Good_Energy/send_energy_Princess.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("My Dear Princess Sanmati !!")
+st.write(
+    "I know you were angry and now you are in No mood",
+  "Here is some good energy, Sending you lots of energy and love"
+)
+
+st.image(r"Send_Good_Energy/baby-bear-brown.gif") 
+st.audio(r"Send_Good_Energy/Hall_Of_Fame.mp3")
