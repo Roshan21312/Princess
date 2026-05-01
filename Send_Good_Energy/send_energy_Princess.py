af2961785dd0d8c05e4ba6560b7e3b71d1f9e48d
Roshan Zambare <124487460+Roshan21312@users.noreply.github.com>
2026-05-01 22:07:38 +0530

    Update send_energy_Princess.py

--- Send_Good_Energy/send_energy_Princess.py
+++ Send_Good_Energy/send_energy_Princess.py
@@ -6,5 +6,5 @@ st.write(
     "Here is some good energy, Sending you lots of energy and love"
 )
 
-st.image(r"Send_Good_Energy/love.gif") 
+st.image(r"Send_Good_Energy/pagal.gif") 
 st.audio(r"Send_Good_Energy/Hall_Of_Fame.mp3")
